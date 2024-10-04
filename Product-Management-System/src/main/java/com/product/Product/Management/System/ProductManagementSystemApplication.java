aa0da0171b02d58448062bee55ebfe8441b5530b
rohangupta029 <rohangupta029@gmail.com>
2024-10-04 12:29:10 +0530

    Project commited 10/04/2024

--- Product-Management-System/src/main/java/com/product/Product/Management/System/ProductManagementSystemApplication.java
+++ Product-Management-System/src/main/java/com/product/Product/Management/System/ProductManagementSystemApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductManagementSystemApplication.class, args);
+
+		 SpringApplication.run(ProductManagementSystemApplication.class, args);
 	}
 
 }
